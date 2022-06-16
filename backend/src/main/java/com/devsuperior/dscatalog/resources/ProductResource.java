58b69c0a53a716c7520dc51c27069b041fce6c81
rafaelspirlandelli <rafaelspirlandelli@hotmail.com>
2022-06-15 22:34:11 -0300

    Tests passing

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -29,7 +29,6 @@ public class ProductResource {
 	@GetMapping
 	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
 		Page<ProductDTO> list = service.findAllPaged(pageable);
-		
 		return ResponseEntity.ok().body(list);
 	}
 	
